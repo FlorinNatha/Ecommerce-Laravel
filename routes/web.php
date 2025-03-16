0425c8474934c685b739319a287654c3bf5ee3ab
florinsilva <121447369+FlorinNatha@users.noreply.github.com>
2025-03-16 16:10:28 +0530

    Delete & Show catagory from admin panel

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,5 @@ route::get('/redirect',[HomeController::class,'redirect']);
 route::get('/view_catagory',[AdminController::class,'view_catagory']);
 
 route::post('/add_catagory',[AdminController::class,'add_catagory']);
+
+route::get('/delete_catagory/{id}',[AdminController::class,'delete_catagory']);
