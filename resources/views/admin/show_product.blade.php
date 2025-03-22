1dcf44107d1fc46ea5c7dbbfeb45395374c3743e
florinsilva <121447369+FlorinNatha@users.noreply.github.com>
2025-03-22 10:24:05 +0530

    Delete and Update Product data

--- resources/views/admin/show_product.blade.php
+++ resources/views/admin/show_product.blade.php
@@ -42,6 +42,14 @@
 
         <div class="main-panel">
             <div class="content-wrapper">
+
+                @if(session()->has('message'))
+                    <div class="alert alert-success">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                        {{session()->get('message')}}
+                    </div>
+                @endif
+
                 <h2 class="font_size">All Product</h2>
                 <table class="center">
                     <tr class="th_color">
@@ -67,8 +75,8 @@
                         <td>
                             <img class="img_size" src="/product/{{$product->image}}">
                         </td>
-                        <td><a class="btn btn-danger" href="{{url('delete_product',$product->id)}}">Delete</a></td>
-                        <td><a class="btn btn-success" href="">Edit</a></td>
+                        <td><a class="btn btn-danger" onclick="return confirm('Are You Sure to Delete this')" href="{{url('delete_product',$product->id)}}">Delete</a></td>
+                        <td><a class="btn btn-success" href="{{url('update_product',$product->id)}}">Edit</a></td>
                     </tr>
                     @endforeach
 
