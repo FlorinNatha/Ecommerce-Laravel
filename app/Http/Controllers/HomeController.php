bf9cd299637798abe333b75619a2e33b3d612d63
florinsilva <121447369+FlorinNatha@users.noreply.github.com>
2025-03-22 23:13:59 +0530

    Adding products to cart

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 
 use App\Models\User;
 use App\Models\Product;
+use App\Models\Cart;
 
 class HomeController extends Controller
 {
@@ -39,5 +40,40 @@ class HomeController extends Controller
         public function product_details($id){
             $product=product::find($id);
             return view('home.product_details',compact('product'));
+        }
+
+        public function add_cart(Request $request, $id){
+            if(Auth::id()){  //user loged welad check karnawa
+                $user= Auth::user(); //userge data gannwa 
+                $product = product::find($id);
+
+                $cart = new cart;
+
+                $cart->name=$user->name;
+                $cart->email=$user->email;
+                $cart->phone=$user->phone;
+                $cart->address=$user->address;
+                $cart->user_id=$user->id;  //'id' eka gaanne databese eke nama save wela tina eka 
+                      //'user_id'  cart table ke dapu eka database eke
+                
+                $cart->product_title=$product->title;
+
+                if($product->discount_price !=null){  //product eke discount price ekk tinwa nm,
+                    $cart->price=$product->discount_price * $request->quantity;  // cart eke save wenne price ekat discount price eka
+                }else{
+                    $cart->price=$product->price * $request->quantity; // else cart eke save wenne product eke atta price eka
+                }
+                
+                $cart->image =$product->image;
+                $cart->product_id=$product->id;
+                //$cart->quantity=$product->quantity;
+                $cart->quantity=$request->quantity;  //'quantity' gththe product.blaade.php wala 'name' kiyala dapu thanin
+
+                $cart->save();
+                return redirect()->back();
+
+            }else{
+                return redirect('login');
+            }
         }
 }
