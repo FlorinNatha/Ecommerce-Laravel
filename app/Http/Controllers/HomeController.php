b17db9e9e420074787eb328ba427859327e23cdf
florinsilva <121447369+FlorinNatha@users.noreply.github.com>
2025-03-23 23:04:44 +0530

    move cart data to order table

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Product;
 use App\Models\Cart;
+use App\Models\Order;
 
 class HomeController extends Controller
 {
@@ -92,5 +93,37 @@ class HomeController extends Controller
             $cart=cart::find($id);
             $cart->delete();
             return redirect()->back();
+        }
+
+        public function cash_order(){
+            $user=Auth::user();  //loged wechcha user data gnnava $user variable ekata
+            $userid=$user->id;
+            $data = cart :: where('user_id', '=', $userid)->get(); //cart table eke user_id eke match wenwad balnwa userge id ekt
+            
+            foreach($data as $data){
+                $order = new order;
+
+                $order->name=$data->name; //mull name eka order table ekn 2weni name eka cart table ekn
+                $order->email=$data->email;
+                $order->phone=$data->phone;
+                $order->address=$data->address;
+                $order->user_id=$data->user_id;
+
+                $order->product_title=$data->product_title;
+                $order->price=$data->price;
+                $order->quantity=$data->quantity;
+                $order->image=$data->image;
+                $order->product_id=$data->product_id;
+
+                $order->payment_status='cash on delivery';
+                $order->delivery_status='processing';
+
+                $order->save();
+
+                $cart_id=$data->id; //cart ekn id eka gnnwa
+                $cart=cart::find($cart_id);
+                $cart->delete();
+            }
+            return redirect()->back()->with('message','We have Received Your Order. We will connect with you soo');
         }
 }
