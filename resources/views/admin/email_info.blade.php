6c04d85d05fe05739cb8234df88fe8e6d92cf9d5
florinsilva <121447369+FlorinNatha@users.noreply.github.com>
2025-05-12 23:55:23 +0530

    updated email notification

--- resources/views/admin/email_info.blade.php
+++ resources/views/admin/email_info.blade.php
@@ -1,9 +1,30 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title></title>
-    </head>
-    <body>
-        <h1>Send Email to user</h1>
-    </body>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+     <base href="/public">
+    @include('admin.css')
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_sidebar.html -->
+      @include('admin.sidebar')
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_navbar.html -->
+        @include('admin.header')
+        <!-- partial -->
+        
+        <div class="main-panel">
+            <div class="content-wrapper">
+            </div>
+        </div>
+
+
+
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    @include('admin.script')
+    <!-- End custom js for this page -->
+  </body>
 </html>
