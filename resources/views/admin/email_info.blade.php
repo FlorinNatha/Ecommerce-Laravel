94c1b486327e8aac0ec65a3db814d8c54d65376f
florinsilva <121447369+FlorinNatha@users.noreply.github.com>
2025-05-19 21:51:50 +0530

    created send email notification

--- resources/views/admin/email_info.blade.php
+++ resources/views/admin/email_info.blade.php
@@ -4,6 +4,16 @@
     <!-- Required meta tags -->
      <base href="/public">
     @include('admin.css')
+
+    <style type="text/css">
+      label{
+        display: inline-block;
+        width: 200px;
+        font-size: 15px;
+        font-weight: bold;
+      }
+    </style>
+
   </head>
   <body>
     <div class="container-scroller">
@@ -17,7 +27,38 @@
         
         <div class="main-panel">
             <div class="content-wrapper">
-                <h1>Send Email to {{$order->email}}</h1>
+                <h1 style="text-align: center; font-size: 25px;">Send Email to {{$order->email}}</h1>
+
+                <form>
+                  <div style="padding-left: 35%; padding-top: 30px;">
+                    <label>Email Greeting :</label>
+                    <input type="text" name="greeting">
+                  </div>
+                  <div style="padding-left: 35%; padding-top: 30px;">
+                    <label>Email FirstLing :</label>
+                    <input type="text" name="firstline">
+                  </div>
+                  <div style="padding-left: 35%; padding-top: 30px;">
+                    <label>Email Body :</label>
+                    <input type="text" name="body">
+                  </div>
+                  <div style="padding-left: 35%; padding-top: 30px;">
+                    <label>Email Button name :</label>
+                    <input type="text" name="button">
+                  </div>
+                  <div style="padding-left: 35%; padding-top: 30px;">
+                    <label>Email Url :</label>
+                    <input type="text" name="url">
+                  </div>
+                  <div style="padding-left: 35%; padding-top: 30px;">
+                    <label>Email Last Line :</label>
+                    <input type="text" name="lastline">
+                  </div>
+                  <div style="padding-left: 35%; padding-top: 30px;">
+                    
+                    <input type="submit" value="Send Email" class="btn btn-primary">
+                  </div>
+                </form>
             </div>
         </div>
 
